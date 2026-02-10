da7c4645b6f758f9809806d7fb3a3d1a0c45d083
srikanthr121 <srikanthr121@gmail.com>
2026-02-10 10:04:58 -0600

    update tailwind import and add safe svg sizing

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -199,7 +199,7 @@ export default function SignUp() {
           type="button"
           className="w-full flex justify-center items-center py-3 px-4 bg-white border border-gray-200 rounded-lg text-slate-700 font-medium hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-200 transition-all duration-200"
         >
-          <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
+          <svg className="w-5 h-5 mr-3" width="20" height="20" viewBox="0 0 24 24">
             <path
               fill="#4285F4"
               d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
