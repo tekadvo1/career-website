85b2973bbe2ec8987bcf685bc13a957cc454d406
srikanthr121 <srikanthr121@gmail.com>
2026-02-25 18:03:10 -0600

    fix: change Start This Project to Continue to Timeline in schedule modal

--- client/src/components/ProjectSetupModal.tsx
+++ client/src/components/ProjectSetupModal.tsx
@@ -229,7 +229,7 @@ export default function ProjectSetupModal({ isOpen, onClose, project, role }: Pr
           >
             {loading
               ? <><div className="w-4 h-4 border-2 border-white/30 border-t-white rounded-full animate-spin" /> Starting…</>
-              : <>Start This Project <ChevronRight className="w-4 h-4" /></>}
+              : <>Continue to Timeline <ChevronRight className="w-4 h-4" /></>}
           </button>
         </div>
 
