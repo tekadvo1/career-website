16181919601c3e6e5fae8642791de9970cae512d
srikanthr121 <srikanthr121@gmail.com>
2026-03-30 15:01:22 -0500

    chore: remove temporary char_test.js and db test scripts

--- backend/check_tables.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgresql://postgres:iXbjqloBGLwGCkoTNFEaPdmurPgePsrR@caboose.proxy.rlwy.net:31652/railway' });
-client.connect()
-  .then(() => client.query('SELECT table_name FROM information_schema.tables WHERE table_schema=\'public\';'))
-  .then(res => { console.log(JSON.stringify(res.rows, null, 2)); client.end(); })
-  .catch(e => { console.error(e); client.end(); });
